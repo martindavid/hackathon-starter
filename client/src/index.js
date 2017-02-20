204a22deb6537a0a6874353bcb2cfc957e6cfb47
Martin David Valentino <martin.david.siagian@gmail.com>
2017-02-20 22:54:44 +1100

    setup store and reducers and create new build

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import './index.scss';
+import configureStore from './store';
+import './index.css';
 import routes from './routes';
 import 'font-awesome/css/font-awesome.css';
 import 'flexboxgrid/css/flexboxgrid.css';
@@ -11,7 +13,11 @@ import 'flexboxgrid/css/flexboxgrid.css';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+const store = configureStore();
+
 ReactDOM.render(
-  <Router routes={routes} history={browserHistory} />,
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory} />
+  </Provider>,
   document.getElementById('root'), // eslint-disable-line no-undef
 );
