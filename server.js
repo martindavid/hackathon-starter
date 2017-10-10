7fda79343f6d4dfc893673e96d9c5b726a1e9d37
Martin David Valentino <martin.david.siagian@gmail.com>
2017-10-10 20:19:06 +1100

    restructure codebase, replace sequelize with mongoose

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const debug = require('debug');
-const entries = require('./api/entries');
 
 const app = express();
+
+/**
+ * Load environment variables from .env file
+ */
+dotenv.load({ path: '.env' });
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
 
 app.set('port', (process.env.PORT || 3001));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(logger('dev'));
+app.use(logger(process.env.NODE_ENV));
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
-
-app.use('/api/entries', entries);
-
-app.post('/*', (req, res) => {
-  res.redirect('/');
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
