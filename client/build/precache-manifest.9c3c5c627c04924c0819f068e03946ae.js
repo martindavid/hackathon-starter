50a813e2d8a46f52709b2b975e239750a0ad884a
Martin Valentino <martin.valentino@cultureamp.com>
2019-09-20 01:35:57 +1000

    revert changes, add build to source control

---
+++ client/build/precache-manifest.9c3c5c627c04924c0819f068e03946ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28eb327b7fedc5970c32e78946e679f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61cc7f4d20c309663c4a",
+    "url": "/static/css/2.3fad095e.chunk.css"
+  },
+  {
+    "revision": "917e809615df14d30575",
+    "url": "/static/css/main.05a5b52f.chunk.css"
+  },
+  {
+    "revision": "61cc7f4d20c309663c4a",
+    "url": "/static/js/2.7a7fc3a1.chunk.js"
+  },
+  {
+    "revision": "917e809615df14d30575",
+    "url": "/static/js/main.706854e5.chunk.js"
+  },
+  {
+    "revision": "8e26cd7931e528114a52",
+    "url": "/static/js/runtime~main.ecbc9411.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
